78d9db3ad52131d03324157a1c7bce660e54e445
kjithing <kjithing@gmail.com>
2021-10-15 22:04:36 +0530

    Update 11.c

--- 11.c
+++ 11.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
-int main() {  
-    char c;
-    printf("Enter a character: ");
-    scanf("%c", &c);  
-    
-    // %d displays the integer value of a character
-    // %c displays the actual character
-    printf("ASCII value of %c = %d", c, c);
-    
+int main() {
+    int dividend, divisor, quotient, remainder;
+    printf("Enter dividend: ");
+    scanf("%d", &dividend);
+    printf("Enter divisor: ");
+    scanf("%d", &divisor);
+
+    // Computes quotient
+    quotient = dividend / divisor;
+
+    // Computes remainder
+    remainder = dividend % divisor;
+
+    printf("Quotient = %d\n", quotient);
+    printf("Remainder = %d", remainder);
     return 0;
 }
