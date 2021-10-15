e26dea8108cc5fa5f938b875d3136057c96d3490
kjithing <kjithing@gmail.com>
2021-10-15 21:58:22 +0530

    Update 11.c

--- 11.c
+++ 11.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+int main() {   
+    int number;
+   
+    printf("Enter an integer: ");  
+    
+    // reads and stores input
+    scanf("%d", &number);
+
+    // displays output
+    printf("You entered: %d", number);
+    
+    return 0;
 }
